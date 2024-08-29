485af137a424a372462e69a32b9515784d2b1abb
QAISER ALI <qaisers.official@gmail.com>
2024-08-29 23:32:39 +0500

    Actually i was working on master branch directly but when i realized then i delete the files and create new branch to proceed

--- Video Camera/code.js
+++
@@ -1,5 +0,0 @@
-const Camera = document.getElementById('camera-Input');
-navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-    Camera.srcObject = stream;
-    Camera.play();
-});
